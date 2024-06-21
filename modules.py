ae3a70f8f5e0c8f17ec4867235f17aa57406bfeb
darkstar69 <thisisdarkstar@duck.com>
2024-06-21 17:13:28 +0530

    fixed final bug

--- modules.py
+++ modules.py
@@ -1,22 +1,15 @@
-# method to convert char to ascii
-def convertToAscii(char):
-    return ord(char)
-
-# method to revert ascii to char
-def convertToChar(ascii):
-    return chr(ascii)
-
 # encrypt text method
-def encrypt_text(string, shift_count):
+def encrypt_text(string, shift):
     encrypted_text = ""
     for s in string:
-        encrypted_text+=convertToChar(convertToAscii(s) + shift_count)
+        if s.isalpha():
+            ascii_offset = 65 if s.isupper() else 97
+            encrypted_text += chr((ord(s) - ascii_offset + shift) % 26 + ascii_offset)
+        else:
+            encrypted_text += s
     return encrypted_text
 
 # decrypt text method      
-def decrypt_text(string, shift_count):
-    decrypted_text = ""
-    for s in string:
-        decrypted_text+=convertToChar(convertToAscii(s) - shift_count)
-    return decrypted_text
+def decrypt_text(string, shift):
+    return encrypt_text(string, -shift)
 
